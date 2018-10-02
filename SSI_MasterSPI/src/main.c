3dd48e6a27ff4248d805a28d8b2d50e71e6ec46c
Alexander S. Lepatenko <Lepatenko@BSVT-NT>
2018-10-02 17:32:42 +0300

    TivaC

--- SSI_MasterSPI/src/main.c
+++ SSI_MasterSPI/src/main.c
@@ -9,59 +9,59 @@
 //! - SSI0Tx  - PA5
 //----------------------------------------------
 
-//---------------------------Настройки LTC2664IUH-12-----------------------------
-// ----------------Span Code----------------
-#define WRITE_SPAN_CMD          0x06
-#define FRAMES_0V_5V            0x00
-#define FRAMES_0V_10V           0x01
-#define FRAMES_M5V_5V           0x02
-#define FRAMES_M10V_10V         0x03
-#define FRAMES_M2_5V_2_5V       0x04
+//--------------------------- LTC2664IUH-12-----------------------------
+#define ADRESS_DAC0      0b00000000
+#define ADRESS_DAC1      0b00000010
+#define ADRESS_DAC2      0b00000100
+#define ADRESS_DAC3      0b00000110
 
-//---------------Analog Mux Control Codes---------------
-#define MUX_DISABLED            00000b
-#define MUX_MUXIN0              10000b
-#define MUX_Vout0               10001b
-#define MUX_Vout1               10010b
-#define MUX_MUXIN1              10011b
-#define MUX_MUXIN2              10100b
-#define MUX_Vout2               10101b
-#define MUX_Vout3               10110b
-#define MUX_MUXIN3              10111b
-#define MUX_REFLO               11000b
-#define MUX_REF                 11001b
-#define MUX_T_M                 11010b
-#define MUX_V_PL                11011b
-#define MUX_V_MN                11100b
-
-//-------------------------------------------------------------------------------
+/*******************COMMAND*******************************/
+#define WRITE_CODE_N                        0b00000000
+#define WRITE_CODE_ALL                      0b10000000
+#define WRITE_SPAN_N                        0b01100000
+#define WRITE_SPAN_ALL                      0b11100000
+#define POWER_UP_UPDATE_N                   0b00010000
+#define POWER_UP_UPDATE_ALL                 0b10010000
+#define WRITE_CODE_N_POWER_UP_UPDATE_N      0b10110000
+#define WRITE_CODE_N_POWER_UP_UPDATE_ALL    0b00100000
+#define WRITE_CODE_ALL_POWER_UP_UPDATE_ALL  0b10100000
+#define POWER_DOWN_N                        0b01000000
+#define POWER_DOWN_CHIP                     0b01010000
+#define ANALOG_MUX                          0b10110000
+#define TOOGLE_SELECT                       0b11000000
+#define GLOBAL_SELECT                       0b11010000
+#define CONFIG                              0b01110000
+#define NO_OP                               0b11110000
 
 
 
+//----------------------------------------------------------------------
 
-/*******************Инициализация периферии*********************/
+
+
+
 void InitPeripheral(void)
 {
-    //Включение тактирования(указываем сист. пределитель 1,использовать  выбранный один из осцил. как систему тактирования, внеш. кварц,частота)
+    //Настройка тактирования MPU
     SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
                                SYSCTL_XTAL_16MHZ);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);//вкл. тактирование перифирии
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI0);//вкл. тактирование SSI
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);//тактирование порта А
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI0);//тактирование интерфейса SSI
 
-    GPIOPinConfigure(GPIO_PA2_SSI0CLK);//настройка пина тактирования CLK
-    GPIOPinConfigure(GPIO_PA3_SSI0FSS);//настройка пина тактирования SS
-    GPIOPinConfigure(GPIO_PA4_SSI0RX); //настройка пина тактирования RX(MISO)
-    GPIOPinConfigure(GPIO_PA5_SSI0TX); //настройка пина тактирования TX(MOSI)
+    GPIOPinConfigure(GPIO_PA2_SSI0CLK);//ножка тактирование
+    GPIOPinConfigure(GPIO_PA3_SSI0FSS);//ножка CS
+    GPIOPinConfigure(GPIO_PA4_SSI0RX); // ножка RX(MISO)
+    GPIOPinConfigure(GPIO_PA5_SSI0TX); // ножка TX(MOSI)
     GPIOPinTypeSSI(GPIO_PORTA_BASE, GPIO_PIN_5 | GPIO_PIN_4 | GPIO_PIN_3 |
-                       GPIO_PIN_2);//настройка пинов SSI(SSI0,получает тактовою частоту MPU,настройка тактирования линии CLK,скорость передачи, размер фрейма)
+                       GPIO_PIN_2);
+    //интерфейс SSI(SSI0,тактирование MPU,полярность  CLK,режим мастер, скорость, формат)
     SSIConfigSetExpClk(SSI0_BASE,SysCtlClockGet(),SSI_FRF_MOTO_MODE_0,SSI_MODE_MASTER,1000000,8);
-    SSIEnable(SSI0_BASE);//вкл.модуля SSI
+    SSIEnable(SSI0_BASE);// Вкл.интерфейс SSI
 
 
 }
 
 
-/****************Прием данных*************************/
 void SSI_GetValueSPI(uint32_t *buf)
 {
     while(SSIDataGetNonBlocking(SSI0_BASE, buf))
@@ -72,7 +72,6 @@ void SSI_GetValueSPI(uint32_t *buf)
 
 
 
-/************************Функция передачи данных(массив)*********************/
 void SSI_SendbufSPI(uint32_t *pBuf, uint32_t *count)
 {
     uint32_t i;
@@ -88,18 +87,28 @@ void SSI_SendbufSPI(uint32_t *pBuf, uint32_t *count)
 }
 
 
-/************************Функция передачи данных(фрейм)*********************/
 void SSI_SendSPI(uint32_t Byte)
 {
     SSIDataPut(SSI0_BASE,Byte);
 }
 
-/************************Функция передачи байта*********************/
+
 void SendByteSPI(uint8_t B)
 {
     SSIDataPut(SSI0_BASE,B);
 }
 
+
+
+void LTC2664IUH_SendCommand(uint8_t Command,uint8_t Address,uint16_t data)
+{
+    uint32_t Bufcommand;
+    uint8_t temp=Command|Address;
+    Bufcommand=(Bufcommand<<24)|temp;
+    Bufcommand|=data;
+    SSI_SendSPI(Bufcommand);
+
+}
 
 
 
@@ -109,7 +118,7 @@ void SendByteSPI(uint8_t B)
 
 void main(void)
 {
-    InitPeripheral();//настройка периферии
+    InitPeripheral();
 
     while(1)
     {
