cdafea5cacbf7179c4c091062ebba3b9c9fd95e8
Alex <dante.l.levi93@gmail.com>
2018-11-11 21:56:33 +0400

    TivaC

--- TEST_PCB/Test-DEBUG_LED_SWITCH/inc/Main.h
+++ TEST_PCB/Test-DEBUG_LED_SWITCH/inc/Main.h
@@ -4,8 +4,6 @@
 
 #include <stdbool.h>
 #include <stdint.h>
-#include "inc/hw_ints.h"
-#include "inc/hw_memmap.h"
 #include "driverlib/gpio.h"
 #include "driverlib/interrupt.h"
 #include "driverlib/pin_map.h"
