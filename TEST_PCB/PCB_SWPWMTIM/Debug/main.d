937301d0efa4b36f2e718bb85b117887f03ddf5d
Alexander S. Lepatenko <Lepatenko@BSVT-NT>
2018-11-13 08:38:17 +0300

    TivaC

--- TEST_PCB/PCB_SWPWMTIM/Debug/main.d
+++ TEST_PCB/PCB_SWPWMTIM/Debug/main.d
@@ -1,6 +1,7 @@
 # FIXED
 
 main.obj: ../main.c
+main.obj: D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdbool.h
 main.obj: D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h
 main.obj: D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h
 main.obj: D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h
@@ -8,13 +9,19 @@ main.obj: D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/includ
 main.obj: D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h
 main.obj: D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h
 main.obj: D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h
-main.obj: D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdbool.h
-main.obj: D:/ti/TIVA/driverlib/gpio.h
+main.obj: D:/ti/TIVA/inc/hw_ints.h
 main.obj: D:/ti/TIVA/inc/hw_memmap.h
-main.obj: D:/ti/TIVA/driverlib/timer.h
+main.obj: D:/ti/TIVA/driverlib/gpio.h
 main.obj: D:/ti/TIVA/driverlib/interrupt.h
+main.obj: D:/ti/TIVA/driverlib/pin_map.h
+main.obj: D:/ti/TIVA/driverlib/sysctl.h
+main.obj: D:/ti/TIVA/driverlib/timer.h
+main.obj: D:/ti/TIVA/driverlib/uart.h
+main.obj: D:/ti/TIVA/utils/uartstdio.h
+main.obj: D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdarg.h
 
 ../main.c: 
+D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdbool.h: 
 D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h: 
 D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h: 
 D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h: 
@@ -22,8 +29,13 @@ D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_typ
 D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h: 
 D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h: 
 D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h: 
-D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdbool.h: 
-D:/ti/TIVA/driverlib/gpio.h: 
+D:/ti/TIVA/inc/hw_ints.h: 
 D:/ti/TIVA/inc/hw_memmap.h: 
-D:/ti/TIVA/driverlib/timer.h: 
+D:/ti/TIVA/driverlib/gpio.h: 
 D:/ti/TIVA/driverlib/interrupt.h: 
+D:/ti/TIVA/driverlib/pin_map.h: 
+D:/ti/TIVA/driverlib/sysctl.h: 
+D:/ti/TIVA/driverlib/timer.h: 
+D:/ti/TIVA/driverlib/uart.h: 
+D:/ti/TIVA/utils/uartstdio.h: 
+D:/Program\ Files/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdarg.h: 
--- TEST_PCB/PCB_SWPWMTIM/main.c
+++ TEST_PCB/PCB_SWPWMTIM/main.c
@@ -1,9 +1,28 @@
-#include <stdint.h>
 #include <stdbool.h>
-#include "driverlib/gpio.h"
+#include <stdint.h>
+#include "inc/hw_ints.h"
 #include "inc/hw_memmap.h"
-#include "driverlib/timer.h"
+#include "driverlib/gpio.h"
 #include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/timer.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
+
+//flag interrupt handler
+static volatile bool g_bIntFlag = false;
+
+
+//handler Interrupt
+void Timer0Isr(void)
+{
+    // Очистка флага прерывания таймера
+    TimerIntClear(TIMER0_BASE, TIMER_TIMB_TIMEOUT);
+    //Toogle GPIO BITs
+
+}
+
 
 
 void Delay(void)
@@ -35,10 +54,25 @@ void SetInputSwitch(void)
 
 
 
+/*********************Config Timer**************************************/
 void Timer_init(void)
 {
-
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+    //Config(name_Timer,(configBits)(две половины длины таймера|половина длины таймера регистр B))
+    TimerConfigure(TIMER0_BASE,TIMER_CFG_SPLIT_PAIR|TIMER_CFG_B_ONE_SHOT);
+    //задание предделителя
+    TimerPrescaleSet(TIMER0_BASE,TIMER_B,16000);
+    //задаем до какого числа будет считать таймер
+    TimerLoadSet(TIMER0_BASE,TIMER_B,1000);
+    //register Interrupt
+    TimerIntRegister(TIMER0_BASE,TIMER_B,Timer0Isr);
+    //вкл. прерываний таймера
+    TimerIntEnable(TIMER0_BASE,TIMER_TIMB_TIMEOUT);
+    IntEnable(INT_TIMER0B);
+    //вкл. Таймер
+    TimerEnable(TIMER0_BASE,TIMER_B);
 }
+
 
 
 void Timer_pwmInit(void)
@@ -49,6 +83,8 @@ void Timer_pwmInit(void)
 
 void main(void)
 {
+    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
+                               SYSCTL_XTAL_16MHZ);
     SetOutPutLED();
     SetInputSwitch();
 
